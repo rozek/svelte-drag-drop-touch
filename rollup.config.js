fd06c2fadb4015e7be54e2cc7d71486f9d733029
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 10:02:19 +0200

    change to build ESM only

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
 // see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
 
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-drag-drop-touch.ts',
   output: {
-    dir: './',
-    format: 'umd',  // builds for both Node.js and Browser
-    name:'Conversion', // required for UMD modules
-    noConflict:true,
-    sourcemap: true,
-    exports: 'default',
+    file:     './svelte-drag-drop-touch.esm.js',
+    format:   'esm',
+    sourcemap:true
   },
-  plugins: [typescript(), terser()],
+  plugins: [
+    commonjs(),
+    resolve(),
+    typescript(),
+    terser()
+  ],
 };
