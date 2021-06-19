dc9288089febeb8afdd4c3018826dc59bc4e11ad
Andreas Rozek <A.Rozek@gmx.de>
2021-06-19 06:19:24 +0200

    revert mistake

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 // see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
 
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
